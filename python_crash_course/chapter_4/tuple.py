1a964757c62ccbe1c98acfaa77f158e6be1d9377
noahsch1998 <schlonoa@gmail.com>
2024-07-08 10:53:23 -0500

    Add files via upload

---
+++ python_crash_course/chapter_4/tuple.py
@@ -0,0 +1,11 @@
+# tuples are lists than can not be changed after creation
+# create them by using parenthesis instead of brackets
+
+dimensions = (200, 50)
+for dimension in dimensions:
+    print(dimension)
+
+# to adjust the touple you muse redefine the entire touple
+dimensions = (250, 75)
+for dimension in dimensions:
+    print(dimension)
