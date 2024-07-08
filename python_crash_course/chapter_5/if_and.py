915bd1166ded58032c2ebb0307e6a39300b74c29
noahsch1998 <schlonoa@gmail.com>
2024-07-08 10:52:22 -0500

    Add files via upload

---
+++ python_crash_course/chapter_5/if_and.py
@@ -0,0 +1,7 @@
+score_0 = 74
+score_1 = 87
+
+if score_0 > 70 and score_1 > 70:
+    print("pass")
+else:
+    print("fail")
---
+++ python_crash_course/chapter_5/if_or.py
@@ -0,0 +1,7 @@
+score_0 = 72
+score_1 = 74
+
+if score_0 > 70 or score_1 > 70:
+    print("pass")
+else:
+    print("fail")
