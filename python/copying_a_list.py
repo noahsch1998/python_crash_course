df332c848d5c782001ed6d418e5e85b81e0434b9
noahsch1998 <schlonoa@gmail.com>
2024-07-08 10:56:07 -0500

    Delete python directory

--- python/copying_a_list.py
+++
@@ -1,6 +0,0 @@
-# to copy a list you can create a slice of the entire list
-# by omitting the first and last index in the slice
-
-count = [num for num in range(1, 11)]
-count_copy = count[:]
-print(count_copy)
--- python/exercises/count_to_twenty.py
+++
@@ -1,4 +0,0 @@
-# use a for loop to print the numbers one to twenty
-
-count = [number for number in range(1, 21)]
-print(count)
